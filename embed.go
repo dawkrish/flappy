e67cca9b2363a620496cd73b1e0f5551d6828abe
krishnanshagarwal112 <gonkillua2680@gmail.com>
2024-04-10 23:24:33 +0530

    embed.go

--- embed.go
+++ embed.go
@@ -5,15 +5,15 @@ import (
 )
 
 var (
-	//go:embed jab.wav
+	//go:embed assets/jab.wav
 	Jab_wav []byte
 
-	//go:embed jump.ogg
+	//go:embed assets/jump.ogg
 	Jump_ogg []byte
 
-	//go:embed gopher.png
+	//go:embed assets/gopher.png
 	Gopher_png []byte
 
-	//go:embed background.png
+	//go:embed assets/background.png
 	Background_png []byte
 )
